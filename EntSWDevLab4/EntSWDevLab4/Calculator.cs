3b1e24e8efd77bcb068d910722f034cc73371e34
Adam McGivern <adammcgivern@outlook.com>
2015-09-24 18:07:09 +0100

    Finished up lab4, which deals with Indexers and Exceptions

---
+++ EntSWDevLab4/EntSWDevLab4/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntSWDevLab4
+{
+   public class Calculator
+    {
+
+        private Calculator()
+        {
+        }
+        public static float Divide(float left, float right)
+        {
+            float divided = 0;
+            if (right != 0)
+            {
+                divided = (left / right);
+            }
+            else
+            {
+                throw (new ArgumentException("Invalid division: 0 is undefined"));
+            }
+            return divided;
+        }
+    }
+}
--- EntSWDevLab4/EntSWDevLab4/Program.cs
+++ EntSWDevLab4/EntSWDevLab4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,36 @@ namespace EntSWDevLab4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            try
+            {
+                string first = "Number1:";
+                string secondnum = "number2: ";
+                Console.WriteLine(first);
+                string firstNumber = Console.ReadLine();
+                float a = float.Parse(firstNumber, NumberStyles.AllowDecimalPoint);
+                Console.WriteLine(secondnum);
+                string second = Console.ReadLine();
+                float b = float.Parse(second, NumberStyles.AllowDecimalPoint);
+                Console.WriteLine(Calculator.Divide(a, b));
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect format for number");
+            }
+            catch (OverflowException e2)
+            {
+                Console.WriteLine(e2);
+            }
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
