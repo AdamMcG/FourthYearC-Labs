0545c5ddbbe0bfe3788a54705cae29961058f3ab
Adam McGivern <adammcgivern@outlook.com>
2015-09-28 20:13:09 +0100

    Begun lab 5. Only problem is that the complexity of the convert method could skyrocket.

---
+++ EntSWDevLab5/EntSWDevLab5/Money.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntSWDevLab5
+{
+    public struct Money
+    {
+        public enum CurrencyTypes {
+            Euro, Dollar, Yen
+        }
+
+        public Money(CurrencyTypes currency, double amount) : this()
+        {
+            this.Currency = Currency;
+            this.Amount = amount;
+        }
+
+        public double Convert(CurrencyTypes tcurrency)
+        {
+            if (tcurrency == Currency)
+            {
+                return Amount;
+            }
+            if (tcurrency == CurrencyTypes.Dollar)
+                return Amount*EuroToDollarRate;
+            else if (tcurrency == CurrencyTypes.Euro)
+                return Amount*DollarToEuroRate;
+            else if (Currency == CurrencyTypes.Dollar)
+                return (Amount*DollarToEuroRate)*EuroToYenRate;
+
+                return Amount*EuroToYenRate;
+        }
+
+        public CurrencyTypes Currency { get; set; }
+        public double Amount { get; set; }
+
+        private const double EuroToDollarRate = 1.12;
+        private const double DollarToEuroRate = 0.89;
+        private const double EuroToYenRate = 134.57;
+
+        public static Money operator +(Money lhs, Money rhs)
+        {
+            return lhs.Currency == rhs.Currency ? new Money(lhs.Currency, lhs.Amount + rhs.Amount) : new Money(lhs.Currency, rhs.Convert(lhs.Currency) + lhs.Amount);
+        }
+    }
+}
---
+++ EntSWDevLab5/EntSWDevLab5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntSWDevLab5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Money m = new Money(Money.CurrencyTypes.Euro, 12333.32);
+            Money m2 = new Money(Money.CurrencyTypes.Yen, 1222222222);
+            Money m3 = new Money(Money.CurrencyTypes.Dollar, 3000.334);
+
+        }
+    }
+}
