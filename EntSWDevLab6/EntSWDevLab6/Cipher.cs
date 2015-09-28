7f989f5f90631f3c510dc98aa29228d76505d0ae
Adam McGivern <adammcgivern@outlook.com>
2015-09-28 21:11:37 +0100

    Completed lab 6. Uses delegates and reverses a list of characters

---
+++ EntSWDevLab6/EntSWDevLab6/Cipher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EntSWDevLab6
+{
+    public class Cipher
+    {
+        public string Cipher1(string plainText)
+        {
+            var cipherText = "";
+            if (plainText != null)
+            {
+                // LINQ version of this foreach loop
+                //cipherText = plainText.Select(c => (char) ((c + 1)%(int) char.MaxValue)).Aggregate(cipherText, (current, c2) => current + c2);
+                foreach (var c in plainText)
+                {
+                    var c2 = (char)( (c + 1) % (int)char.MaxValue);
+                    cipherText = cipherText + c2;
+                }
+            }
+            return cipherText;
+        }
+
+        public string Cipher2(string plainText)
+        {
+            Debug.Assert(plainText != null, "plainText != null");
+            var arr = plainText.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
---
+++ EntSWDevLab6/EntSWDevLab6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EntSWDevLab6
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var myChCipher = new Cipher();
+            EncryptDelegate a = myChCipher.Cipher1;
+            EncryptDelegate a2 = myChCipher.Cipher2;
+
+
+
+            Console.WriteLine(a("abz"));
+            Console.WriteLine(a2("abcde"));
+            Console.ReadLine();
+        }
+
+        private delegate string EncryptDelegate(string plainText);
+    }
+}
