efb8c1be4232be97071a59b5ba79312e3c8dd48a
Adam McGivern <adammcgivern@outlook.com>
2015-10-06 23:49:47 +0100

    Completed Lab7. Added some Unit tests to test out functionality, and to practice unit testing.

---
+++ EntSWDevLab7/Lab7 Unit Testingh/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EntSWDevLab7;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab7_Unit_Testingh
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Student myStudent = new Student("X00098814", "Adam", "Trans Male");
+            Assert.AreEqual(myStudent.Name,"Adam");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            StudentClass myClassOfStudents = new StudentClass("C2334", @"Software Development 2");
+            for (int i = 0; i < 10; i++)
+            {
+                myClassOfStudents.AddStudent(new Student("1233", "Testing" + i, "Male"));
+            }
+
+            foreach (var student in myClassOfStudents.Students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
---
+++ EntSWDevLab7/TestCaseForLab7/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCaseForLab7
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Student newStudent = new Student();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
